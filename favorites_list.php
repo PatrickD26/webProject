fa3eddc81a4d2f83f32361c544e5e8082450666c
pdelap26 <delaportepatrick@ymail.com>
2019-12-17 14:31:06 +0100

    wording on files

--- favorites_list.php
+++ favorites_list.php
@@ -11,7 +11,7 @@
 
     getListeSeries();
     ?>
-    <link rel="stylesheet" href="main.css"/>
+    <link rel="stylesheet" href="favorites_list.css"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <title>Series</title>
 </head>
