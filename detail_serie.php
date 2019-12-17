51c91a91a54b6287ebbada6d821170071b477bc6
nhoarau <nathan.hoarau11@gmail.com>
2019-12-17 17:11:25 +0100

    fonctionnel

--- detail_serie.php
+++ detail_serie.php
@@ -3,16 +3,19 @@ if (session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 require("liste_serie.php");
+require("functions.php");
 
 $serie = [];
 $id = $_GET["id"];
 
+
 foreach($_SESSION["shows_all"] as $all)
 {
-    if($all["id"] == $id){
+    if($all["id"] == $id ){
         $serie = $all;
     }
 }
+
 
 ?>
 
@@ -24,24 +27,77 @@ foreach($_SESSION["shows_all"] as $all)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Detail</title>
+    <link rel="stylesheet" href="details.css"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
 </head>
 <body>
-<table>
-    <tr>
-        <td><img src="<?php echo $serie['imgDetail']; ?>" alt=""></td>
-        <td>
-            <tr><h3><?php echo $serie['titre']; ?></h3></tr>
-            <tr><?php echo $serie['type']; ?></tr>
-            <tr><?php echo $serie['resume']; ?></tr>
-        </td>
-        <td>
-            <ul>
-                <li></li>
-            </ul>
-            
-        </td>
-    </tr>
-</table>
-    
+<header>
+    <div class="headerDesktop">
+        <div class="firstColumn">
+            <div class="logo">
+                <div class="logoLabel">Logo</div>
+            </div>
+            <div class="desktopTitle">Liste des séries</div>
+        </div>
+        <div class="secondColumn">
+            Mes favoris | Consulter la liste des séries |
+        </div>
+    </div>
+    <div class="mobileHeader">
+        <div class="logo">
+            <div class="logoLabel">Logo</div>
+        </div>
+        <div class="burger">
+            <i class="material-icons" style="font-size:-webkit-xxx-large">
+                menu
+            </i>
+        </div>
+    </div>
+</header>
+<div>
+    <table class="detail">
+     
+        <form method="post">
+            <input type="submit" name="addFav" value="⭐">
+            <input type="number" name="note" min="0" max="5" placeholder="<?php echo $serie["note"]?>">
+        <form>
+            <td rowspan="2"><img src="<?php echo $serie['imgDetail']; ?>" alt=""></td>
+        </tr>
+        <tr>
+            <td>
+                <tr><h3><?php echo $serie['titre']; ?></h3></tr>
+                <tr>type: <?php echo $serie['type']; ?></tr><br><br>
+                <tr><?php echo $serie['resume']; ?></tr>
+            </td>
+            <td <?php foreach($serie['episodes'] as $saison){?>>
+                <ul>
+                    <li><?php echo $saison["saison"] ?>
+                        <ul>
+                        <?php foreach($saison['episodes'] as $episode) {?>
+                            <li>
+                                <?php echo $episode; ?>
+                                </li>
+                        <?php }?>
+                        </ul>
+                    </li>
+                </ul>
+            <?php }; ?>  
+            </td>
+        </tr>
+
+       
+    </table>
+</div>
 </body>
 </html>
+
+
+<?php
+ if (isset($_POST['addFav'])) {
+    updateFavorites($id);
+ }
+ if (isset($_POST['note'])) {
+    updateNoteSerie($id, $_POST['note']);
+ }
+?>
--- functions.php
+++ functions.php
@@ -8,30 +8,30 @@ include_once("liste_serie.php");
 // Ajouter/Retirer une serie des favoris
 function updateFavorites($id) {
     
-    foreach($_SESSION["shows_json"] as $key => $value)
+    foreach($_SESSION["shows_all"] as $key => $value)
     {
         if($id == $value["id"])
         {
-            $value["favoris"] == 1 ? $_SESSION["shows_json"][$key]["favoris"] = 0 : $_SESSION["shows_json"][$key]["favoris"] = 1;
+            $value["favoris"] == 1 ? $_SESSION["shows_all"][$key]["favoris"] = 0 : $_SESSION["shows_all"][$key]["favoris"] = 1;
         }
     }
 
-    file_put_contents('liste_serie.json', json_encode($_SESSION["shows_json"]));
+    file_put_contents('liste_serie.json', json_encode($_SESSION["shows_all"]));
 
     getListeSeries();
 }
 
 // Mise à jour de la note d'une serie
 function updateNoteSerie($id, $note) {
-    foreach($_SESSION["shows_json"] as $key => $value)
+    foreach($_SESSION["shows_all"] as $key => $value)
     {
         if($id == $value["id"])
         {
-            $_SESSION["shows_json"][$key]["note"] = $note;
+            $_SESSION["shows_all"][$key]["note"] = $note;
         }
     }
 
-    file_put_contents('liste_serie.json', json_encode($_SESSION["shows_json"]));
+    file_put_contents('liste_serie.json', json_encode($_SESSION["shows_all"]));
 
     getListeSeries();
 }
