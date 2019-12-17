85f8fd5b0f15c6f53e936618e5e1c6c426fc4f4c
pdelap26 <delaportepatrick@ymail.com>
2019-12-17 12:11:07 +0100

    working listing

--- main.php
+++ main.php
@@ -7,7 +7,7 @@
     <?php
     session_start();
     //récupération de la liste des séries
-        include_once("liste_serie.php");
+    include_once("liste_serie.php");
     ?>
     <link rel="stylesheet" href="main.css"/>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -15,29 +15,32 @@
 </head>
 <body>
 <table>
-<header>
-<div class="mobileHeader">
-<div class="firstRow">
-<div class="logo">
-<div class="logoLabel">Logo</div>
-</div>
-<div class="burger">
-<i class="material-icons" style="font-size:-webkit-xxx-large">
-menu
-</i>
-</div>
-</div>
-<div class="secondRow">
-Mes favoris
-</div>
-</div>
-</header>
-<?php
-foreach($_SESSION["shows_json"] as $value)
-?>
-<tr>
-<td><?php var_dump($value); ?></td>
-</tr
+    <header>
+        <div class="mobileHeader">
+            <div class="firstRow">
+                <div class="logo">
+                    <div class="logoLabel">Logo</div>
+                </div>
+                <div class="burger">
+                    <i class="material-icons" style="font-size:-webkit-xxx-large">
+                        menu
+                    </i>
+                </div>
+            </div>
+            <div class="secondRow">
+                Mes favoris
+            </div>
+        </div>
+    </header>
+    <?php
+    foreach ($_SESSION["shows_json"] as $value):
+        ?>
+        <tr>
+            <td><img src="<?php  ?>" alt=""></td>
+            <td><?php printf($value['titre']); ?></td>
+        </tr>
+    <?php endforeach; ?>
+
 </table>
 
 </body>
