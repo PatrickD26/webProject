f20216f83ca93529e1d60f8ea46dade3140dfc9c
pdelap26 <delaportepatrick@ymail.com>
2019-12-17 11:10:41 +0100

    session started verification

--- liste_serie.php
+++ liste_serie.php
@@ -1,7 +1,8 @@
 
 <?php
-session_start();
-
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
 $file = file_get_contents('liste_serie.json');
 
 $jsons = json_decode($file, true);
